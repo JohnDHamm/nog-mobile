d05aa0a8ce0bc530aa4702cb849290e6553d0641
John Hamm <johndhamm1@gmail.com>
2017-10-09 13:08:51 -0500

    home screen styling - nog logo and user info

--- src/screens/home.js
+++ src/screens/home.js
@@ -18,12 +18,12 @@ export default class Home extends React.Component {
 						style={styles.nogLogo}
 						source={require('../img/nog_logo_whiteOnRed.png')} />
 				</View>
-				<View>
-					<Text style={styles.userName}>{TestUser.name}</Text>
+				<View style={styles.userView}>
 					<Image
 						style={styles.profileImg}
 						resizeMode={'contain'}
 						source={require('../img/buddy_elf.png')} />
+					<Text style={styles.userName}>{TestUser.name}</Text>
 				</View>
 				<Button
 					onPress={() => this.props.navigation.navigate('NogSelect')}
@@ -42,19 +42,30 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: values.nogRed
+		backgroundColor: values.nogRed,
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingTop: 40
 	},
 	nogTitle: {
 		flexDirection: 'row',
 		justifyContent: 'center',
-		width: 200
 	},
 	nogLogo: {
-		// height: 200,
+		//orig size - 813x627@300ppi
+		width: 813 / 627 * 150,
+		height: 150,
+	},
+	userView: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 20
 	},
 	userName: {
 		color: 'white',
-		fontSize: 20
+		fontSize: 25,
+		paddingLeft: 10
 	},
 	profileImg: {
 		width: 50,
