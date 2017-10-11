643a6fe9fc3d3df80faae95ed67b4e9d2cae58a1
John Hamm <johndhamm1@gmail.com>
2017-10-11 01:07:30 -0500

    styling of pattern playback + added play/pause btn

--- src/screens/patternPlayback.js
+++ src/screens/patternPlayback.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button, Slider } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Button, Slider, Image } from 'react-native';
+import values from '../styles/values';
 
 export default class PatternMultiColor extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			playing: false
+		}
+	}
 
 	componentDidMount() {
 		// console.log("this.props.navigation.state.params", this.props.navigation.state.params);
@@ -15,43 +23,103 @@ export default class PatternMultiColor extends React.Component {
 		console.log("color slider val", val);
 	}
 
-	startPattern() {
-		console.log("start the magic via bluetooth");
+	togglePlayPause() {
+		this.state.playing ? console.log("playing via bluetooth") : console.log("paused");
+		this.setState({ playing: !this.state.playing })
 	}
 
 	render() {
 		const pattern = this.props.navigation.state.params;
 
 		return (
-			<View>
-				{pattern.singleColor ?
-					<Text>single color</Text> : <Text>multi-color</Text>}
-				<Text>{pattern.description}</Text>
-				<View>
-					<Text>speed</Text>
-					<Slider
-						minimumValue={1}
-						maximumValue={100}
-						step={1}
-						value={pattern.defaultSpeed}
-						onValueChange={this.speedSliderChange}
-						/>
-				</View>
-				{pattern.singleColor &&
+			<View style={styles.container}>
+				<View style={styles.topBlock}>
+					<Text style={styles.description}>{pattern.description}</Text>
 					<View>
-						<Text>color</Text>
+						<Text style={styles.label}>speed</Text>
 						<Slider
 							minimumValue={1}
 							maximumValue={100}
 							step={1}
-							value={pattern.defaultColor}
-							onValueChange={this.colorSliderChange}
+							value={pattern.defaultSpeed}
+							minimumTrackTintColor={values.nogGreen}
+							onValueChange={this.speedSliderChange}
 							/>
-					</View>}
-				<Button
-					onPress={this.startPattern}
-					title='Play' />
+					</View>
+					{pattern.singleColor &&
+						<View>
+							<Text style={styles.label}>color</Text>
+							<Slider
+								minimumValue={1}
+								maximumValue={100}
+								step={1}
+								value={pattern.defaultColor}
+								minimumTrackTintColor={values.nogGreen}
+								onValueChange={this.colorSliderChange}
+								/>
+						</View>}
+				</View>
+
+				<View style={styles.bottomBlock}>
+					{ !this.state.playing ?
+						<TouchableOpacity
+							onPress={() => this.togglePlayPause()} >
+							<Image
+								source={require('../img/playBtn.png')}
+								style={styles.playBtn} />
+						</TouchableOpacity>
+						:
+						<TouchableOpacity
+							onPress={() => this.togglePlayPause()} >
+							<Image
+								source={require('../img/pauseBtn.png')}
+								style={styles.playBtn} />
+						</TouchableOpacity>
+					}
+				</View>
+
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 10,
+		paddingLeft: 15,
+		paddingRight: 15,
+		paddingBottom: 10
+	},
+	topBlock: {
+
+	},
+	description: {
+		fontSize: 20,
+		paddingBottom: 10
+	},
+	label: {
+		fontSize: 15,
+		color: values.nogGrayText,
+		fontStyle: 'italic'
+	},
+	bottomBlock: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	playBtn: {
+		width: 100,
+		height: 100
+	}
+
+
+})
+
+
+
+
+
+
+
+
